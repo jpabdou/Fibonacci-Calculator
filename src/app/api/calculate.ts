119e0f0c673dcb979c4110ea2ea07e5be86365f8
jab <johnpabdou@gmail.com>
2023-08-01 23:29:52 -0700

    One more comment to calculate.tsx for possible error that can occur and how it could be corrected

--- src/app/api/calculate.ts
+++ src/app/api/calculate.ts
@@ -5,6 +5,7 @@ export function calculation (n_value : number, array : FibonacciData[]) {
     // If the array already contains the target length (n_value), then the function is terminated with no modification to the array.
     // Otherwise, a while loop is started with the calculation of the next Fibonacci number calculated based on the sum of the last two numbers. 
     // This result is added to the array as a data object and the loop continues until it reaches the target array length
+    // Only risk of this logic is if earlier Fibonacci number entries were deleted for any reason. Can include a step where the function iterates over the entire array to check for missing entries, but it does add more time and make the calculation more costly.
     while (array.length < n_value) {
         let index : number = array.length-1;
         let newFibonacci : number = array[index].fibonacci_number + array[index-1].fibonacci_number;
